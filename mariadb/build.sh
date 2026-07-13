2e51f79e830a9323130d072ae6359ebe223faabb
Boris Rybalkin <ribalkin@gmail.com>
2026-07-13 20:31:14 +0100

    mariadb: wrap real 11.4 binaries (mariadbd/mariadb) with musl loader so mariadb-install-db works

--- mariadb/build.sh
+++ mariadb/build.sh
@@ -9,11 +9,10 @@ mkdir -p ${BUILD_DIR}
 cp -r /usr ${BUILD_DIR}
 cp -r /lib ${BUILD_DIR}
 
-mv ${BUILD_DIR}/usr/bin/resolveip ${BUILD_DIR}/usr/bin/resolveip.bin
-mv ${BUILD_DIR}/usr/bin/my_print_defaults ${BUILD_DIR}/usr/bin/my_print_defaults.bin
-mv ${BUILD_DIR}/usr/bin/mysqld ${BUILD_DIR}/usr/bin/mysqld.bin
-mv ${BUILD_DIR}/usr/bin/mysql ${BUILD_DIR}/usr/bin/mysql.bin
-mv ${BUILD_DIR}/usr/bin/mariadb-dump ${BUILD_DIR}/usr/bin/mariadb-dump.bin
+cd ${BUILD_DIR}/usr/bin
+rm -f mysqld mysql mysqldump
+for b in mariadbd mariadb mariadb-dump my_print_defaults resolveip; do
+  mv $b $b.bin
+done
 
 cp ${DIR}/bin/* ${BUILD_DIR}/usr/bin
-
