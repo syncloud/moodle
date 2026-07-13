eb40e14fa8535c910bf218c5925d5cc99ae4d0be
Boris Rybalkin <ribalkin@gmail.com>
2026-07-13 19:36:57 +0100

    mariadb: correct basedir and plugin-dir to $SNAP/mariadb/usr layout

--- bin/service.mariadb.sh
+++ bin/service.mariadb.sh
@@ -4,7 +4,7 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
 rm -rf $SNAP_DATA/database/aria_log_control
 export MYSQL_HOME=$SNAP_DATA/config
 exec ${DIR}/mariadb/usr/bin/mysqld \
-  --basedir=$SNAP_DATA/mariadb/usr \
+  --basedir=$SNAP/mariadb/usr \
   --datadir=$SNAP_DATA/database \
-  --plugin-dir=$SNAP/mariadb/lib/plugin \
+  --plugin-dir=$SNAP/mariadb/usr/lib/mariadb/plugin \
   --pid-file=$SNAP_DATA/database/mariadb.pid
